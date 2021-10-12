9d887787b743502cd024628f9e22dd729a66fadf
James Baird <jbaird@clever-touch.com>
2021-10-12 18:58:38 +0100

    adusted models

--- tasks/models.py
+++ tasks/models.py
@@ -1,16 +1,23 @@
 from django.db import models
 
+STATUS_CHOICES = (
+    ('active','Active'),
+    ('With client', 'With Client'),
+    ('blocked','BLOCKED'),
+)
+
+
 class Tasks(models.Model):
     """A typical class defining a model, derived from the Model class."""
 
     # Fields
     task_id = models.AutoField(primary_key=True)
     task_name = models.TextField(blank=True, null=True)
-    client = models.TextField(blank=True, null=True)
+    client = models.CharField(max_length=40, blank=True, null=True)
     start_date = models.DateTimeField(blank=True)
     end_date = models.DateTimeField(blank=True)
-    status = models.TextField(blank=True, null=True)
-    owner = models.TextField(blank=True, null=True)
+    status = models.CharField(max_length=40, choices=STATUS_CHOICES, default='active')
+    owner = models.CharField(max_length=40, blank=True, null=True)
 
     # Metadata
     class Meta:
